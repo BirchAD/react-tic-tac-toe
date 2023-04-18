977a5e36cc032e5e7f2bc823ad424067715136ae
BirchAD <andrewbirch1882@icloud.com>
2023-04-18 08:42:00 +0100

    Completed up to alternative X / O

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+
+function Square({ value, onClick }) {
+  return (
+    <button
+      className="square"
+      onClick={onClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  function handleClick(i) {
+    const newSquares = [...squares];
+    console.log(newSquares)
+    newSquares[i] = xIsNext ? 'X' : 'O';
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  function renderSquare(i) {
+    return (
+      <Square
+        value={squares[i]}
+        onClick={() => handleClick(i)}
+      />
+    );
+  }
+
+  const status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+
+  return (
+    <>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </>
+  );
+}
